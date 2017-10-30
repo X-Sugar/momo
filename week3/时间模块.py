49bc7233a3b3e818b7c295d1c2734c43fd9f869a
Sugar <xiaosubao@qq.com>
2017-10-30 11:02:31 +0800

    version1.0

--- week3/时间模块.py
+++ week3/时间模块.py
@@ -9,7 +9,7 @@ import time
 # print(time.time())  #1509023231.7808106 时间戳
 # time.sleep(3)
 # print(time.clock()) #计算cpu执行的时间
-# print(time.gmtime())  #英国时间
+# print(time.gmtime())  #UTC时间
 # print(time.localtime()) #本地时间
 # print(help(time.strftime))
 # print(time.strftime('%Y-%m-%d %H:%M:%S'))   #字符串时间
@@ -24,5 +24,5 @@ import time
 
 # print(time.mktime(time.localtime()))    #转换成时间戳.
 
-import datetime
-print(datetime.datetime.now())  #2017-10-26 22:00:24.462707
+# import datetime
+# print(datetime.datetime.now())  #2017-10-26 22:00:24.462707
