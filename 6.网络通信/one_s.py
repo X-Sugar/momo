600c4145a995dd9dd6cd619abaa21671749e7492
Color <xiaosubao@qq.com>
2017-12-19 23:25:15 +0800

    111

--- 6.网络通信/one_s.py
+++ 6.网络通信/one_s.py
@@ -8,15 +8,20 @@ sk = socket.socket()
 sk.bind(('127.0.0.1',8888))
 sk.listen(3)
 
-conn,addr=sk.accept()
-
 while True:
-    recv_data = str(conn.recv(1024),'utf8')
-    print('接收的消息：%s' %recv_data)
+    conn,addr=sk.accept()
+    print(addr)
+    while True:
+        try:
+            data = conn.recv(1024)
+            # recv_data = str(conn.recv(1024),'utf8')
+        except Exception:
+            break
+        print('接收的消息：%s' % str(data,'utf8'))
 
-    send_data = input('输入发送内容：')
-    if recv_data == 'exit':
-        break
-    conn.send(bytes(send_data,'utf8'))
+        send_data = input('输入发送内容：')
+        if send_data == 'exit':
+            break
+        conn.send(bytes(send_data,'utf8'))
 
-sk.close()
+    sk.close()
