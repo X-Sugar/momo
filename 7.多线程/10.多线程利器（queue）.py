a1d68c483ff0cc2724b7c553160e5bf466c16f35
Sugar <xiaosubao@qq.com>
2018-01-08 19:35:27 +0800

    version19

--- 7.多线程/10.多线程利器（queue）.py
+++ 7.多线程/10.多线程利器（queue）.py
@@ -46,57 +46,142 @@
 
 # one -------------------------------------------------------
 
-# import threading,queue
-# from time import sleep
-# from random import randint
-#
-# class Production(threading.Thread):
-#     def run(self):
-#         while True:
-#             r = randint(0,100)
-#             q.put(r)
-#             print("生产出来的%s号包子" % r)
-#             sleep(1)
-#
-# class Proces(threading.Thread):
-#     def run(self):
-#         while True:
-#             re=q.get()
-#             print("吃掉的%s号包子" % re)
-#
-# if __name__ == '__main__':
-#     q=queue.Queue(10)
-#     threads = [Production(),Production(),Production(),Proces()]
-#     for t in threads:
-#         t.start()
+import threading,queue
+from time import sleep
+from random import randint
+
+class Production(threading.Thread):
+    def run(self):
+        while True:
+            r = randint(0,100)
+            q.put(r)
+            print("生产出来的%s号包子" % r)
+            sleep(1)
+
+class Proces(threading.Thread):
+    def run(self):
+        while True:
+            re=q.get()
+            print("吃掉的%s号包子" % re)
+
+if __name__ == '__main__':
+    q=queue.Queue(2)
+    threads = [Production(),Production(),Production(),Proces()]
+    for t in threads:
+        t.start()
 
 # two ------------------------------------------------------
 
-import random,time,queue,threading
+# import random,time,queue,threading
+#
+# q = queue.Queue()
+#
+# def Producer(name):
+#     count = 0
+#     while count < 20:
+#         time.sleep(random.randrange(3))
+#         q.put(count)
+#         print("生产者%s已经生产了%s个包子" % (name,count))
+#         count += 1
+#
+# def Consumer(name):
+#     count = 0
+#     while count < 20:
+#         time.sleep(random.randrange(4))
+#         if not q.empty():
+#             data = q.get()
+#             print(data)
+#             print("%s吃了%s个包子" % (name,count))
+#         else:
+#             print("没有包子了！")
+#         count += 1
+#
+# p1 = threading.Thread(target=Producer,args=('A'))
+# c1 = threading.Thread(target=Consumer,args=('B'))
+# p1.start()
+# c1.start()
 
-q = queue.Queue()
+# threen ------------------------------------------------------------
 
-def Producer(name):
-    count = 0
-    while count < 20:
-        time.sleep(random.randrange(3))
-        q.put(count)
-        print("生产者%s已经生产了%s个包子" % (name,count))
-        count += 1
+"""
+# 实现一个线程不断生成一个随机数到一个队列中(考虑使用Queue这个模块)
+# 实现一个线程从上面的队列里面不断的取出奇数
+# 实现另外一个线程从上面的队列里面不断取出偶数
 
-def Consumer(name):
-    count = 0
-    while count < 20:
-        time.sleep(random.randrange(4))
-        if not q.empty():
-            data = q.get()
-            print(data)
-            print("%s吃了%s个包子" % (name,count))
-        else:
-            print("没有包子了！")
-        count += 1
+import random,threading,time
+from queue import Queue
 
-p1 = threading.Thread(target=Producer,args=('A'))
-c1 = threading.Thread(target=Consumer,args=('B'))
-p1.start()
-c1.start()
+# Producer thread
+class Producer(threading.Thread):
+    def __init__(self, t_name, queue):
+        threading.Thread.__init__(self,name=t_name)
+        self.data=queue
+
+    def run(self):
+        for i in range(10):     # 随机产生10个数字 ，可以修改为任意大小
+            randomnum = random.randint(1,99)
+            print("%s: %s is producing %d to the queue!" % (time.ctime(), self.getName(), randomnum))
+            self.data.put(randomnum)    # 将数据依次存入队列
+            time.sleep(1)
+
+        print("%s: %s finished!" % (time.ctime(), self.getName()))
+
+# Consumer thread
+class Consumer_even(threading.Thread):
+    def __init__(self,t_name,queue):
+        threading.Thread.__init__(self,name=t_name)
+        self.data=queue
+
+    def run(self):
+        while 1:
+            try:
+                val_even = self.data.get(1,5) # get(self, block=True, timeout=None) ,1就是阻塞等待,5是超时5秒
+                if val_even%2 == 0:
+                    print ("%s: %s is consuming. %d in the queue is consumed!" % (time.ctime(),self.getName(),val_even))
+                    time.sleep(2)
+                else:
+                    self.data.put(val_even)
+                    time.sleep(2)
+            except:   # 等待输入，超过5秒 就报异常
+                print("%s: %s finished!" %(time.ctime(),self.getName()))
+                break
+
+class Consumer_odd(threading.Thread):
+    def __init__(self,t_name,queue):
+        threading.Thread.__init__(self, name=t_name)
+        self.data=queue
+    def run(self):
+        while 1:
+            try:
+                val_odd = self.data.get(1,5)
+                if val_odd % 2 != 0:
+                    print("%s: %s is consuming. %d in the queue is consumed!" % (time.ctime(), self.getName(), val_odd))
+                    time.sleep(2)
+                else:
+                    self.data.put(val_odd)
+                    time.sleep(2)
+            except:
+                print ("%s: %s finished!" % (time.ctime(), self.getName()))
+                break
+
+# Main thread
+def main():
+    queue = Queue()
+    
+    producer = Producer('随机数.', queue)
+    consumer_even = Consumer_even('偶数', queue)
+    consumer_odd = Consumer_odd('奇数',queue)
+    
+    producer.start()
+    consumer_even.start()
+    consumer_odd.start()
+    
+    producer.join()
+    consumer_even.join()
+    consumer_odd.join()
+    
+    print('All threads terminate!')
+
+if __name__ == '__main__':
+    main()
+"""
