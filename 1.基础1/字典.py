0d2f55861f3aebe53a61273b2351c43fff1c75da
Color <xiaosubao@qq.com>
2018-04-22 15:00:58 +0800

    112

--- 1.基础1/字典.py
+++ 1.基础1/字典.py
@@ -5,7 +5,7 @@
 dic={1:'alex','age':35,'hobby':{'girl_name':'铁锤','age':45},'is_handsome':True}
 # dic={'name':'alex','age':35,'hobby':{'girl_name':'铁锤','age':45},'is_handsome':True}
 
-print(dic)
+# print(dic)
 
 # # 字典两大特点：无序，键唯一
 
@@ -53,13 +53,13 @@ print(dic)
 # dic3['age']=55
 # print(dic3)
 #
-# dic4={'age': 18, 'name': 'alex', 'hobby': 'girl'}
-# # dic5={'1':'111','2':'222'}
-# dic5={'1':'111','name':'222'}
-#
-# dic4.update(dic5)
-# print(dic4)
-# print(dic5)
+dic4={'age': 18, 'name': 'alex', 'hobby': 'girl'}
+# dic5={'1':'111','2':'222'}
+dic5={'1':'111','name':'222'}
+
+dic4.update(dic5)
+print(dic4)
+print(dic5)
 
 
 
@@ -87,7 +87,7 @@ print(dic)
 #
 #
 # dic6=dict.fromkeys(['host1','host2','host3'],'test')
-# print(dic6)#{'host3': 'test', 'host1': 'test', 'host2': 'test'}
+# print(dic6)                         # {'host3': 'test', 'host1': 'test', 'host2': 'test'}
 #
 # dic6['host2']='abc'
 # print(dic6)
@@ -170,3 +170,242 @@ print(dic)
 #不用get
 # dic={5:'555',2:'666',4:'444'}
 # print(dic[5])
+
+
+
+# 字典
+# dict
+# dict
+# dic = {
+#     "k1": 'v1',
+#     "k2": 'v2'
+# }
+# 1 根据序列，创建字典，并指定统一的值
+# v = dict.fromkeys(["k1",123,"999"],123)
+# print(v)
+
+# 2 根据Key获取值，key不存在时，可以指定默认值（None）
+# v = dic['k11111']
+# print(v)
+# v = dic.get('k1',111111)
+# print(v)
+
+# 3 删除并获取值
+# dic = {
+#     "k1": 'v1',
+#     "k2": 'v2'
+# }
+# v = dic.pop('k1',90)
+# print(dic,v)
+# k,v = dic.popitem()
+# print(dic,k,v)
+
+# 4 设置值，
+# 已存在，不设置，获取当前key对应的值
+# 不存在，设置，获取当前key对应的值
+# dic = {
+#     "k1": 'v1',
+#     "k2": 'v2'
+# }
+# v = dic.setdefault('k1111','123')
+# print(dic,v)
+
+# 5 更新
+# dic = {
+#     "k1": 'v1',
+#     "k2": 'v2'
+# }
+# dic.update({'k1': '111111','k3': 123})
+# print(dic)
+# dic.update(k1=123,k3=345,k5="asdf")
+# print(dic)
+
+# 6 keys()  7 values()   8 items()   get   update
+##########
+
+
+
+# 1、基本机构
+# info = {
+#     "k1": "v1", # 键值对
+#     "k2": "v2"
+# }
+#### 2 字典的value可以是任何值
+# info = {
+#     "k1": 18,
+#     "k2": True,
+#     "k3": [
+#         11,
+#         [],
+#         (),
+#         22,
+#         33,
+#         {
+#             'kk1': 'vv1',
+#             'kk2': 'vv2',
+#             'kk3': (11,22),
+#         }
+#     ],
+#     "k4": (11,22,33,44)
+# }
+# print(info)
+
+####  3 布尔值(1,0)、列表、字典不能作为字典的key
+# info ={
+#     1: 'asdf',
+#     "k1": 'asdf',
+#     True: "123",
+#     # [11,22]: 123
+#     (11,22): 123,
+#     # {'k1':'v1'}: 123
+#
+# }
+# print(info)
+
+# 4 字典无序
+
+# info = {
+#     "k1": 18,
+#     "k2": True,
+#     "k3": [
+#         11,
+#         [],
+#         (),
+#         22,
+#         33,
+#         {
+#             'kk1': 'vv1',
+#             'kk2': 'vv2',
+#             'kk3': (11,22),
+#         }
+#     ],
+#     "k4": (11,22,33,44)
+# }
+# print(info)
+
+# 5、索引方式找到指定元素
+# info = {
+#     "k1": 18,
+#     2: True,
+#     "k3": [
+#         11,
+#         [],
+#         (),
+#         22,
+#         33,
+#         {
+#             'kk1': 'vv1',
+#             'kk2': 'vv2',
+#             'kk3': (11,22),
+#         }
+#     ],
+#     "k4": (11,22,33,44)
+# }
+# # v = info['k1']
+# # print(v)
+# # v = info[2]
+# # print(v)
+# v = info['k3'][5]['kk3'][0]
+# print(v)
+
+# 6 字典支持 del 删除
+# info = {
+#     "k1": 18,
+#     2: True,
+#     "k3": [
+#         11,
+#         [],
+#         (),
+#         22,
+#         33,
+#         {
+#             'kk1': 'vv1',
+#             'kk2': 'vv2',
+#             'kk3': (11,22),
+#         }
+#     ],
+#     "k4": (11,22,33,44)
+# }
+# del info['k1']
+#
+# del info['k3'][5]['kk1']
+# print(info)
+
+# 7 for循环
+# dict
+# info = {
+#     "k1": 18,
+#     2: True,
+#     "k3": [
+#         11,
+#         [],
+#         (),
+#         22,
+#         33,
+#         {
+#             'kk1': 'vv1',
+#             'kk2': 'vv2',
+#             'kk3': (11,22),
+#         }
+#     ],
+#     "k4": (11,22,33,44)
+# }
+# for item in info:
+#     print(item)
+#
+# for item in info.keys():
+#     print(item)
+
+# for item in info.values():
+#     print(item)
+
+# for item in info.keys():
+#     print(item,info[item])
+
+# for k,v in info.items():
+#     print(k,v)
+
+# True 1  False 0
+# info ={
+#     "k1": 'asdf',
+#     True: "123",
+#     # [11,22]: 123
+#     (11,22): 123,
+#     # {'k1':' v1'}: 123
+#
+# }
+# print(info)
+
+#######################　整理 #################
+
+# 一、数字
+# int(..)
+# 二、字符串
+# replace/find/join/strip/startswith/split/upper/lower/format
+# tempalte = "i am {name}, age : {age}"
+# # v = tempalte.format(name='alex',age=19)
+# v = tempalte.format(**{"name": 'alex','age': 19})
+# print(v)
+# 三、列表
+# append、extend、insert
+# 索引、切片、循环
+# 四、元组
+# 忽略
+# 索引、切片、循环         以及元素不能被修改
+# 五、字典
+# get/update/keys/values/items
+# for,索引
+
+# dic = {
+#     "k1": 'v1'
+# }
+
+# v = "k1" in dic
+# print(v)
+
+# v = "v1" in dic.values()
+# print(v)
+# 六、布尔值
+# 0 1
+# bool(...)
+# None ""  () []  {} 0 ==> False
