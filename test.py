e82916ea863f3da2e6f93d9cae38d5429d5b684e
Color <xiaosubao@qq.com>
2018-04-23 23:01:20 +0800

    113

--- test.py
+++ test.py
@@ -1,12 +1,46 @@
-import time
+def login():
+    print('这是登陆功能！')
 
-def show_time():
-    def inner():
-        start = time.time()
-        time.sleep(1)
-        end = time.time()
-        print('spend %s' % (end - start))
-    # return inner
-    print(inner)
 
-show_time()
+def select():
+    print('这是查询功能！')
+
+
+def update():
+    print('这是更新功能！')
+
+
+def delete():
+    print('这是删除功能！')
+
+
+def exit():
+    print('这是退出功能！')
+
+
+if __name__ == '__main__':
+    msg='''
+        1.登陆
+        2.查询
+        3.更新
+        4.删除
+        5.退出
+    '''
+    msg_dic={
+        '1':login,
+        '2':select,
+        '3':update,
+        '4':delete,
+        '5':exit
+    }
+
+    while True:
+        print(msg)
+        choice = input('请输入你的选项：').strip()
+        if not choice:continue
+        if choice == '5':break
+        if choice in msg_dic:
+            res=msg_dic[choice]
+            res()
+        else:
+            print('error')
