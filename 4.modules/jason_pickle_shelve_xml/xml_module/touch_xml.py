6f48a8ff300faa656a5320f6a4c4ac5faf7461bd
Sugar <xiaosubao@qq.com>
2018-04-27 19:08:58 +0800

    version26

--- 4.modules/jason_pickle_shelve_xml/xml_module/touch_xml.py
+++ 4.modules/jason_pickle_shelve_xml/xml_module/touch_xml.py
@@ -9,6 +9,7 @@ name = ET.SubElement(new_xml, "name", attrib={"enrolled": "yes"})
 age = ET.SubElement(name, "age", attrib={"checked": "no"})
 sex = ET.SubElement(name, "sex")
 age.text = '33'
+
 name2 = ET.SubElement(new_xml, "name", attrib={"enrolled": "no"})
 age = ET.SubElement(name2, "age")
 age.text = '19'
